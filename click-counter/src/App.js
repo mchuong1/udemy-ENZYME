d293fb5284df1ef3dd9ececc5e098c759ece823a
Matthew <matthew_chuong@passportinc.com>
2021-04-12 13:48:13 -0400

    Create test
    - red and green first test
    - include babel-plugin-react-remove-properties

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -3,20 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = React.useState(0);
 
   return (
     <div data-test="component-app" className="App">
-      <h1 data-test="counter-display">
-        The counter is currently&nbsp; 
-        <span data-test="count">{count}</span>
-      </h1>
-      <button
-        data-test="increment-button"
-        onClick={() => setCount(count + 1)}
-      >
-        Increment counter
-      </button>
+      <h1>App</h1>
     </div>
   );
 }
