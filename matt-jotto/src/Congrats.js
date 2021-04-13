9c1d87828ba1714234cb59370a2969ef9a7b831a
Matthew <matthew_chuong@passportinc.com>
2021-04-13 14:42:07 -0400

    Create congrats component

--- matt-jotto/src/Congrats.js
+++ matt-jotto/src/Congrats.js
@@ -1,5 +1,16 @@
 //recieve the success state as a prop
 
-export default function Congrats({ success }) {
-  return <div />
+export default function Component(props) {
+
+  const congratsMessage = props.success 
+                          ? <span data-test='congrats-message'>
+                              Congratulations! You guessed the word
+                            </span>
+                          : ''
+
+  return (
+    <div data-test="component-congrats">
+      { congratsMessage }
+    </div>
+  )
 }
