891be31b935e43d04f6adc6cf9bf58d02471e20a
Matthew <matthew.chuong@passportinc.com>
2021-06-04 10:33:00 -0400

    Creat mock test for getSecretWord

--- matt-jotto/src/App.js
+++ matt-jotto/src/App.js
@@ -1,14 +1,20 @@
+import { useEffect } from 'react'
 import './App.css';
 
 import Congrats from './Congrats'
 import GuessedWords from './GuessedWords'
 import Input from './Input'
+import { getSecretWord } from './actions'
 
 function App() {
 
   const success = false
   const secretWord = 'party'
   const guessedWord = [];
+
+  useEffect(() => {
+    getSecretWord();
+  }, []);
 
   return (
     <div className="container" data-test="component-app">
--- matt-jotto/src/App.test.js
+++ matt-jotto/src/App.test.js
@@ -1,11 +1,15 @@
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import { findByTestAttr } from '../test/testutil'
 import App from './App';
 
-
+//activate global mock to make sure getSecretWord doesn;t make network call
+jest.mock('./actions')
+import { getSecretWord as mockGetSecretWord } from './actions';
 
 const setup = () => {
-  return shallow(<App />)
+  // `useEffect` not called on `shallow`, use mount
+  // [https://github.com/airbnb/enzyme/issues/2086]
+  return mount(<App />)
 }
 
 test('renders without error', () => {
@@ -13,3 +17,25 @@ test('renders without error', () => {
   const appComponent = findByTestAttr(wrapper, 'component-app')
   expect(appComponent).toHaveLength(1)
 });
+
+describe('get secret word', () => {
+  beforeEach(() => {
+    // clear mock calls
+    mockGetSecretWord.mockClear();
+  })
+  test('get secret word on app mount', () => {
+    const wrapper = setup();
+
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+  test('getSecretWord does not run on app update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+
+    // using setProps because wrapper.update() doesn't trigger useEffect
+    // https://hithub.com/enzymejs/enzyme/issues/2254
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
+})
