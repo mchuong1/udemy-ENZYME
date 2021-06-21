b661832f65e12531b29d94dc03f08528d0522506
Matthew <matthew.chuong@passportinc.com>
2021-06-21 11:20:56 -0400

    Add guessWords reducer and action
    - remove correctGuess function and test

--- matt-jotto/src/actions/index.js
+++ matt-jotto/src/actions/index.js
@@ -2,11 +2,19 @@ import axios from 'axios'
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD',
 }
 
-export const correctGuess = () => {
-  return { type: actionTypes.CORRECT_GUESS };
-} 
+export const guessWord = (guessedWord) => {
+  return function(dispatch, getState) {
+
+  };
+};
+
+// Deprecated
+// export const correctGuess = () => {
+//   return { type: actionTypes.CORRECT_GUESS };
+// } 
 
 export const getSecretWord = () => {
   // TODO: write actual action in Redux / context section
