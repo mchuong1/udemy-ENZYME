46e78f87a64f8e514487ac8fc5217d02b3127d56
Matthew <matthew.chuong@passportinc.com>
2021-06-23 11:08:28 -0400

    Add logic to integration tests
    add logic to actions
    add logic to guessedWordsReducer
    create secretWordReducer

--- matt-jotto/src/actions/index.js
+++ matt-jotto/src/actions/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import { getLetterMatchCount } from '../helpers'
+
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
@@ -7,7 +9,17 @@ export const actionTypes = {
 
 export const guessWord = (guessedWord) => {
   return function(dispatch, getState) {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord)
 
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount }
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORRECT_GUESS})
+    }
   };
 };
 
