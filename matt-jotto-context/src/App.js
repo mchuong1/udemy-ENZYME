a89efe6ae40bc26c1ed3f6ecf98a2c4109bcc091
Matthew <matthew.chuong@passportinc.com>
2021-06-29 09:24:01 -0400

    Implement language context in Congrats component

--- matt-jotto-context/src/App.js
+++ matt-jotto-context/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import './App.css';
 
 import Congrats from './Congrats';
--- matt-jotto-context/src/Congrats.js
+++ matt-jotto-context/src/Congrats.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+
+import languageContext from './context/languageContext'
+import stringsModule from './helpers/strings'
 
 /**
  * Functional react component for congratulatory message.
@@ -8,11 +11,13 @@ import PropTypes from 'prop-types';
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
  */
 const Congrats = (props) => {
+  const language = useContext(languageContext);
+
   if (props.success) {
     return (
       <div data-test="component-congrats" className="alert alert-success">
         <span data-test="congrats-message">
-          Congratulations! You guessed the word!
+          {stringsModule.getStringByLanguage(language, 'congrats')}
         </span>
       </div>
     );
